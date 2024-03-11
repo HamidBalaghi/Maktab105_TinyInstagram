84cb596b5c7222a11b2c3c6408dc0c0ac04350e8
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-12 01:42:56 +0330

    🧩 add Profile and Follow models to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile, Follow
 
 
 class UserAccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number', 'email', 'is_active', 'is_admin')
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'is_active', 'is_public')
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'following', 'accepted')
+
+
 admin.site.register(User, UserAccountAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Follow, FollowAdmin)
