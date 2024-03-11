d46fd729e2546fed9857d05f39bdda23d992d17b
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-11 15:28:15 +0330

    ⚙ Edit User model , USERNAME_FIELD = 'email'

--- accounts/models.py
+++ accounts/models.py
@@ -1,41 +1,30 @@
 from django.core.validators import RegexValidator
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, phone_number, email=None, name=None, password=None):
-        if not phone_number:
-            raise ValueError('User must have a phone number')
-        email = self.normalize_email(email)
-        user = self.model(phone_number=phone_number, email=email, name=name)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, phone_number, email=None, name=None, password=None):
-        user = self.create_user(phone_number, email, name, password)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 
 
 class User(AbstractBaseUser):
-    phone_number = models.CharField(max_length=11, unique=True, validators=[
+    email = models.CharField(max_length=100, unique=True, validators=[
+        RegexValidator(
+            regex='^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+            message='Enter a valid email address'
+        )
+    ])
+    phone_number = models.CharField(max_length=11, unique=True, null=True, blank=True, validators=[
         RegexValidator(
             regex='^09\d{9}$',
             message='Phone number must be 11 digits starting with "09"'
         )
     ])
-    email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ['email', 'name']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number', 'name']
 
     def __str__(self):
         return self.phone_number
