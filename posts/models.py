830cc2fae8c2fff2991f5c5db8490ef76f1d6572
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-12 13:08:30 +0330

    🧩 Add initial Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from accounts.models import Profile
+
+
 # Create your models here.
+
+
+class Post(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    publish_at = models.DateTimeField(auto_now_add=True)
+    publishable = models.BooleanField(default=True)
