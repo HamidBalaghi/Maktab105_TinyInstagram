6d0b9b6b2f755a22c9fb21538cc84a91aaa4e85b
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-12 14:12:06 +0330

    🧩 add initial 'Comment' model

--- reactions/models.py
+++ reactions/models.py
@@ -10,3 +10,12 @@ class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     liked = models.BooleanField(default=False)
     disliked = models.BooleanField(default=False)
+
+
+class Comment(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    is_deleted = models.BooleanField(default=False)
