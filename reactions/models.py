b30c99ee786c2846547195b857e0904e86c81c48
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-12 13:41:54 +0330

    🧩 add initial 'Like' model

--- reactions/models.py
+++ reactions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import Profile
+from posts.models import Post
+
 
 # Create your models here.
+
+class Like(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    liked = models.BooleanField(default=False)
+    disliked = models.BooleanField(default=False)
