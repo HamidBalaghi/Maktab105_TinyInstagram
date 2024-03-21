4e1b0e0d63539ec9487ee5ad5eeebeeadb3ff04d
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-21 21:55:01 +0330

    🧩 Add Post and Image models to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Image
+
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'publish_at', 'is_active', 'publishable')
+    ordering = ('-publish_at',)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('post',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Image, ImageAdmin)
