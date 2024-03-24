294932c95147eb1d327c4125f45c71056aa5f8c2
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-24 12:35:06 +0330

    🎥 Create initial View for User Activation

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,20 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from .forms import CustomSignUpForm, CustomUserLoginForm
+from django.views.generic import CreateView, FormView
+from .forms import CustomSignUpForm, CustomUserLoginForm, VerifyForm
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from .models import User
 
 
 # Create your views here.
 class SignUpView(CreateView):
     form_class = CustomSignUpForm
-    success_url = reverse_lazy('accounts:login')  # Redirect to login page upon successful signup
     template_name = 'account/signup.html'
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.save()
+        return redirect('accounts:activation', pk=user.pk)
 
 
 class CustomUserLoginView(View):
@@ -24,11 +28,8 @@ class CustomUserLoginView(View):
         form = CustomUserLoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data.get('email')
-            print(email)
             password = form.cleaned_data.get('password')
-            print(password)
             user = authenticate(request, email=email, password=password)
-            print(user)
             if user is not None:
                 login(request, user)
                 return redirect('accounts:signup')  # Redirect to the desired page after login
@@ -40,5 +41,27 @@ class CustomUserLoginView(View):
 class CustomUserLogoutView(View):
     def get(self, request, *args, **kwargs):
         logout(request)
-        # Redirect to the desired page after logout
         return redirect('accounts:login')
+
+
+class UserActivationView(FormView):
+    template_name = 'account/verification.html'
+    form_class = VerifyForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.pk = self.kwargs['pk']
+        self.new_user = User.objects.get(pk=self.pk)
+        if self.new_user.is_active:
+            return redirect('accounts:login')
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        code = form.cleaned_data.get('code')
+        if code == str(self.new_user.otp):
+            self.new_user.is_active = True
+            self.new_user.save()
+            login(self.request, self.new_user)
+            return redirect('accounts:login')  ## todo : i will redirect user to profile
+        else:
+            form.add_error(None, 'Invalid code')
+            return self.form_invalid(form)
