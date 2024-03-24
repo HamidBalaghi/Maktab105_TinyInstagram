9cd4ab3d38510b318bddd1279b659f4c0387da51
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-24 12:22:56 +0330

    ⚙ 📧 Add Email settings

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
+
+## ----EMAIL ----##
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'balaghi.hamid.django@gmail.com'
+EMAIL_HOST_PASSWORD = 'wuod wbbt mlgr focy'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
