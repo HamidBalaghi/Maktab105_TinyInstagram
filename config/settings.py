82794d1ecd7de6cccce66b080ac2bb7e434c3095
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-25 01:24:19 +0330

    📎 Introducing NoneActiveDeleter Middleware to settings

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts.middleware.NoneActiveDeleterMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
