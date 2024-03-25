8b7305244628fbf505c8ad397a4868f432e32e8a
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-25 14:25:40 +0330

    🛠 Add edit profile URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.CustomUserLoginView.as_view(), name='login'),
     path('logout/', views.CustomUserLogoutView.as_view(), name='logout'),
-    path('activation/<int:pk>', views.UserActivationView.as_view(), name='activation')
+    path('activation/<int:pk>', views.UserActivationView.as_view(), name='activation'),
+    path('editprofile/<int:pk>', views.EditProfileView.as_view(), name='editprofile'),
+    path('test/', views.test, name='test')
 ]
