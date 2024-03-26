80394b21f80b73342d385c2208585af14eb13ee8
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-27 02:22:58 +0330

    ⚙ Delete Middleware from settings

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'accounts.middleware.NoneActiveDeleterMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
