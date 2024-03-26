46928c9a69c36ec8b92364d31d99dd0ef6d180d1
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-26 11:41:24 +0330

    ⚙ Add STATICFILES_DIRS to settings

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'  # Corrected STATIC_URL to include a leading slash
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
