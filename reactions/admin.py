6dfb544ed6896886688ecd9c050f5085811fcc36
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-29 01:00:20 +0330

    🧩 Add Like and Comment models to Admin panel

--- reactions/admin.py
+++ reactions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Like, Comment
+
 
 # Register your models here.
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'post', 'liked', 'disliked')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'post', 'parent_comment', 'is_deleted')
+
+
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Comment, CommentAdmin)
