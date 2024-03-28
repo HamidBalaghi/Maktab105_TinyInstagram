485cb6dc9130db1740e5078571687f0736fe2b44
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-29 01:04:01 +0330

    🧩 Add related_name to Like and comment models

--- reactions/models.py
+++ reactions/models.py
@@ -7,14 +7,14 @@ from posts.models import Post
 
 class Like(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reactions')
     liked = models.BooleanField(default=False)
     disliked = models.BooleanField(default=False)
 
 
 class Comment(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
