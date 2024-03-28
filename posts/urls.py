d5f0c812f9b7044264fc4e841463efae700ded81
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-29 01:07:11 +0330

    🛠 Add Post Detail URL

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,6 @@ app_name = 'post'
 
 urlpatterns = [
     path('newpost/<int:pk>', views.NewPostView.as_view(), name='create_post'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post')
 
 ]
