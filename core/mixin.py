df9d44d3b903a0ea19efaaba6430a6d20b093157
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-03-30 01:40:06 +0330

    🌐 Create LoginRequiredMixin

--- core/mixin.py
+++ core/mixin.py
@@ -1,8 +1,8 @@
-# from django.shortcuts import redirect
-#
-#
-# class LoginRequiredMixin:
-#     def dispatch(self, request, *args, **kwargs):
-#         if not request.user.is_authenticated:
-#             return redirect('accounts:login')
-#         return super().dispatch(request, *args, **kwargs)
+from django.shortcuts import redirect
+
+
+class LoginRequiredMixin:
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('accounts:login')
+        return super().dispatch(request, *args, **kwargs)
