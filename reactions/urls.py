f323c3ca60ea86ac2ba273d373d1f413b155c62f
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-01 01:52:42 +0330

    ⚒ Add Follow/Unfollow URLs

--- reactions/urls.py
+++ reactions/urls.py
@@ -6,4 +6,6 @@ app_name = 'reactions'
 urlpatterns = [
     path('like/post/<int:pk>', views.LikeView.as_view(), name='like'),
     path('dislike/post/<int:pk>', views.LikeView.as_view(), name='dislike'),
+    path('follow/<int:pk>', views.FollowView.as_view(), name='follow_user'),
+    path('unfollow/<int:pk>', views.FollowView.as_view(), name='unfollow_user'),
 ]
