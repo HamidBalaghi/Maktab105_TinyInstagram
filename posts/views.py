66900c6bbad6ba358afe97ff310cfd4bdca39050
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-01 14:23:05 +0330

    🎥 Add Initial view for comment

--- posts/views.py
+++ posts/views.py
@@ -7,6 +7,8 @@ from .forms import NewPostForm
 from accounts.models import Profile
 from .models import Post, Image
 from core.mixin import LoginRequiredMixin
+from reactions.forms import NewCommentForm
+from reactions.models import Comment
 
 
 class NewPostView(LoginRequiredMixin, FormView):
@@ -45,7 +47,7 @@ class PostDetailView(LoginRequiredMixin, DetailView):
         if isinstance(request.user, AnonymousUser):
             return redirect('accounts:login')
         elif (not self.get_object().publishable
-              or (request.user != self.owner
+              or (request.user.profile != self.owner
                   and not self.get_object().is_active)
               or not self.owner.is_active):
             raise PermissionDenied
@@ -59,8 +61,24 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         post = self.get_object()
+        context['comment_form'] = NewCommentForm()
         context['comments'] = post.get_comments()
         context['reactions'] = post.get_reactions()
         like_reaction = post.user_reaction(self.request.user.profile)
         context[like_reaction] = True
         return context
+
+    def post(self, request, *args, **kwargs):
+        comment_form = NewCommentForm(request.POST)
+        if comment_form.is_valid():
+            text = comment_form.cleaned_data['text']
+            parent_id = comment_form.cleaned_data['parent']
+
+            new_comment = Comment(profile=request.user.profile, post=self.get_object(), comment=text)
+            if parent_id == 'None':
+                new_comment.save()
+
+        else:
+            print('invalid (test)')
+
+        return redirect('post:post', pk=self.kwargs['pk'])
