cfebc37041a10545db4cd464125345d60715c68a
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-02 12:05:15 +0330

    ⚒ Add Home URL

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,7 @@ app_name = 'post'
 
 urlpatterns = [
     path('newpost/<int:pk>', views.NewPostView.as_view(), name='create_post'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post')
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post'),
+    path('', views.HomeView.as_view(), name='home'),
 
 ]
