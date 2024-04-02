b927c9ea81ad0de442f2f91b56c6cd352dce9cd2
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-02 14:38:07 +0330

    🌐 Create Navbar Mixin

--- core/mixin.py
+++ core/mixin.py
@@ -48,3 +48,13 @@ class ProfilePermissionMixin:
                 context['posts'] = owner.posts.filter(publishable=True)
                 context['has_perm'] = True
         return context
+
+
+class NavbarMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user.profile
+        context['logged_in_user'] = user
+        context['logged_in_user_pk'] = user.pk
+        context['logged_in_user_image'] = user.get_image_url
+        return context
