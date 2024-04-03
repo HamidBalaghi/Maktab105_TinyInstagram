60b79968f5077f4609898484a73b1aa0c136eac4
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-03 22:32:32 +0330

    ⚒ Add Explore URL

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('newpost/<int:pk>', views.NewPostView.as_view(), name='create_post'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post'),
     path('', views.HomeView.as_view(), name='home'),
-
+    path('explore/', views.ExploreView.as_view(), name='explore'),
 ]
