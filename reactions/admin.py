6788949ea08afbabf49f8ab03744d47570cdb3ae
HamidBalaghi <Balaghi.hamid.72@gmail.com>
2024-04-05 23:05:52 +0330

    🧩 Add Hashtag Admin

--- reactions/admin.py
+++ reactions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Like, Comment
+from .models import Like, Comment, Hashtag
 
 
 # Register your models here.
@@ -12,5 +12,10 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('profile', 'post', 'parent_comment', 'is_deleted')
 
 
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
 admin.site.register(Like, LikeAdmin)
 admin.site.register(Comment, CommentAdmin)
+admin.site.register(Hashtag, HashtagAdmin)
